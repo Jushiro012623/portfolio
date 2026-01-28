4a00b79b38518d578e7d74854a65ba698e879a87
Ivan <ivanallen64@gmail.com>
2026-01-28 09:55:04 +0800

    Update index.tsx

--- src/sections/hero/index.tsx
+++ src/sections/hero/index.tsx
@@ -4,7 +4,7 @@ const HeroSection = () => {
         <section className="h-[85vh] min-h-[570px] flex justify-center relative z-10">
             <div className="w-2xl m-auto">
                 <p className="text-5xl px-5 md:px-0 text-center text-primary font-semibold ">
-                    {data.name}
+                    {data.career_role}
                 </p>
                 <span className={`block h-10 w-full `} />
                 <p className="text-xl text-center text-secondary px-5 md:px-0  mx-auto">
